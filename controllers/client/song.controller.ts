a62d3174bafed995de93f8ffb1a5b10ed3a67796
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-06-16 20:26:12 +0700

    trang danh sách bài hát-khung tĩnh-client

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -1,8 +1,14 @@
 import { Response, Request } from "express";
 
+// models
+import Song from "../../models/song.model";
+import Singer from "../../models/singer.model";
+
 export const index = async (req: Request, res: Response) => {
     try{
-        res.send('Danh sacash bài nhạc');
+        res.render('client/pages/songs/index', {
+            title: "Danh sách bài nhạc"
+        })
     }
     catch(error){
 
