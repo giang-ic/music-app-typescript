7663c50c57f9ea6a30c604018681b5be4d380d59
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-06-16 19:32:05 +0700

    trang chủ đề bài hát-khung động ~ đổ data-client

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -5,13 +5,24 @@ import Topic from "../../models/topic.model";
 
 export const index = async (req: Request, res: Response) => {
     try{
-        const topics = await Topic.find({
+        interface findInterface {
+            title?: string,
+            description?: string,
+            status: string,
+            slug?: string,
+            deleted: Boolean
+        }
+
+        const findObject : findInterface = {
+            status: "active",
             deleted: false
-        });
-        console.log(topics);
+        }
+        
+        const topics = await Topic.find(findObject);
         
         res.render('client/pages/topics/index', {
-            title: "Danh mục bài hát"
+            title: "Danh mục bài hát",
+            topics
         });
     }
     catch(error){
