5a9fbb3401da71b4520c3ebc1e7e6f22c81e677d
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-06-16 16:51:30 +0700

    cấu trúc file cơ bản với router

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Music App Config");
+import express, {Express, Response, Request} from "express";
+
+const app: Express = express();
+
+const port:(string | number) = 3000;
+
+app.get(
+    '/topics', 
+    (req: Request, res: Response):void => {
+        console.log("music app topics");
+        res.send("TOPICS");
+    }
+);
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
