8dd162787590989271ad3c92b35c672182095556
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-07-07 01:18:30 +0700

    giao diện tạo bài nhạc mới

--- controllers/admin/song.controller.ts
+++ controllers/admin/song.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 
 // model
 import Song from "../../models/song.model";
+import Topic from "../../models/topic.model";
+import Singer from "../../models/singer.model";
 
 // [GET] /admin/songs/
 export const index = async (req: Request, res: Response) => {
@@ -14,6 +16,30 @@ export const index = async (req: Request, res: Response) => {
         res.render('admin/pages/songs/index', {
             title: "Danh sách bài nhạc",
             songs
+        });
+    }
+    catch(error){
+
+    }
+}
+
+// [GET] /admin/songs/create
+export const createUI = async (req: Request, res: Response) => {
+    try{
+        const topics = await Topic.find({
+            status: "active",
+            deleted: false
+        }).select("title");
+
+        const singers = await Singer.find({
+            status: "active",
+            deleted: false
+        }).select('fullName');
+
+        res.render('admin/pages/songs/create', {
+            title: "Tạo bài nhạc mới",
+            topics,
+            singers
         });
     }
     catch(error){
--- routes/admin/song.router.ts
+++ routes/admin/song.router.ts
@@ -6,9 +6,16 @@ const router: Router = Router();
 // controller
 import * as controller from "../../controllers/admin/song.controller";
 
+// use
 router.get(
     '/',
     controller.index
 );
 
+router.get(
+    '/create',
+    controller.createUI
+);
+
+// export
 export const SongRouter: Router = router;
