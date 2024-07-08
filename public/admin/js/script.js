21e34ca2c99069fac01eea5c83fab8ffb5ef70c8
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-07-08 12:42:59 +0700

    thêm nút tắt image preview

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,16 +1,30 @@
+
+
 // upload preview image
 const uploadImage = document.querySelector('[upload-image]');
 if(uploadImage){
     const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
     const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
+    const closeImage = uploadImage.querySelector("[close-image]");
 
     uploadImageInput.addEventListener("change", (event) => {
         const image = event.target.files[0]; // corresponding: uploadImageInput.files[0]
         
         if(image){
             uploadImagePreview.src = URL.createObjectURL(image);
+            closeImage.classList.toggle("hide");
         }
     });
+
+    // close image
+    if(closeImage){
+        closeImage.addEventListener("click", (event) => {
+            closeImage.classList.add("hide");
+            uploadImagePreview.src = "";
+            uploadImageInput.value = "";
+        });
+    }
+    // end close image
 }
 // end upload preview image
 
