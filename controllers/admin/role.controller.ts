cc0c3f9c591b36540735ea041264bdb296b94aff
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-07-08 19:09:25 +0700

    đảy data ra trang nhóm quyền

--- controllers/admin/role.controller.ts
+++ controllers/admin/role.controller.ts
@@ -6,9 +6,13 @@ import { Response, Request } from "express";
 // [GET] /admin/roles
 export const index = async (req: Request, res: Response) => {
     try{
+        const roles = await Role.find({
+            deleted: false
+        });
+
         res.render("admin/pages/roles/index", {
             title: "Nhóm quyền",
-            
+            roles
         })
     }
     catch(error){
