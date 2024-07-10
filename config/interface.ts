24df44ef5e799a5f4930329f3227c8e8b39b8a6e
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-07-10 16:46:00 +0700

    bộ lọc trạng thái

---
+++ config/interface.ts
@@ -0,0 +1,14 @@
+// model interface
+export interface findTopicInterface {
+    status?: string,
+    deleted: boolean
+}   
+
+
+// interface filter status 
+export interface filterStatusInterface {
+    name: string,
+    status: string,
+    class?: string
+}
+// end interface filter status 
--- controllers/admin/topic.controller.ts
+++ controllers/admin/topic.controller.ts
@@ -3,33 +3,36 @@ import { Response, Request } from "express";
 // model 
 import Topic from "../../models/topic.model";
 
+// interface
+import { findTopicInterface, filterStatusInterface } from "../../config/interface";
+
+// helper
+import * as filterHelper from "../../helper/filter.helper";
+
 // [GET] /admin/topics/
 export const index = async (req: Request, res: Response) => {
     try{
-        interface findInterfaceTopic {
-            status?: string,
-            deleted: boolean
-        }   
-        
-        const findObjectTopic: findInterfaceTopic = {
+        const findObjectTopic: findTopicInterface = {
             deleted: false
         };
 
         // Filter stautus
-        const status: string = `${req.query.status}`;
-        if(status){
-            findObjectTopic["status"] = status;
+        const status: string = `${req.query.status}` || "";
+        if(req.query.status){    
+            findObjectTopic["status"] = status;// không gán biến status vào vì nó sẽ bị tìm theo status = ""
         }
+        const filterStatusArray: filterStatusInterface[] = filterHelper.status(req.query);
         // End Filter stautus
         
         const topics = await Topic.find(findObjectTopic);
 
         res.render('admin/pages/topics/index', {
             title: "Quản lý chủ đề bài hát",
-            topics
+            topics,
+            filterStatusArray
         })
     }
     catch(error){
-
+        console.log(error);
     }
 }
